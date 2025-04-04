f9cdcfce974021b4caf7346175ef67a2ff664864
vedgit872 <vedsmith18@gmail.com>
2025-04-04 21:00:01 +0530

    Cluster Map Changes
    Center the coordinates to india

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -3,7 +3,7 @@ maptilersdk.config.apiKey = maptilerApiKey;
 const map = new maptilersdk.Map({
     container: 'cluster-map',
     style: maptilersdk.MapStyle.BRIGHT,
-    center: [-103.59179687498357, 40.66995747013945],
+    center: [79.0833, 21.0833],
     zoom: 3
 });
 
